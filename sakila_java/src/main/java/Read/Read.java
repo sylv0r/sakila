36b8d627c78f5f746183c14ce8f7cc75ec04e442
Psycockaque <113451954+Psycockaque@users.noreply.github.com>
2023-01-25 14:40:18 +0100

    Read

--- sakila_java/src/main/java/Read/Read.java
+++
@@ -1,5 +0,0 @@
-package Read;
-
-public class Read {
-    
-}
--- sakila_java/src/main/java/com/codingf/Main.java
+++ sakila_java/src/main/java/com/codingf/Main.java
@@ -1,16 +1,13 @@
 package com.codingf;
 
 import com.codingf.db.Query;
+import com.codingf.read.Read;
 
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Query selectQuery = new Query("db");
-        ResultSet rs = selectQuery.executeQuery("SELECT * FROM country");
-        while (rs.next()) {
-            String city = rs.getString("country");
-            System.out.println(city);
+        Read.read("country","country");
         }
     }
-}
+
